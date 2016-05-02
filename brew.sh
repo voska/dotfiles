9c00a4fd1ec3ce2649bb174fda9f25e3cdeb6d00
Matt Voska <mattvoska@gmail.com>
2016-05-01 19:35:24 -0700

    Brew file done!

--- brew.sh
+++ brew.sh
@@ -13,8 +13,6 @@ brew upgrade --all
 brew install coreutils
 ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
@@ -25,76 +23,58 @@ brew install gnu-sed --with-default-names
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
+brew install zsh
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
-
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
 brew install hydra
-brew install john
 brew install knock
-brew install netpbm
 brew install nmap
-brew install pngcheck
-brew install socat
 brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+
+# Install clis for hosting
+brew install heroku-toolbelt
+brew install awscli
+
+# Install databases
+brew install mysql
+brew install sqlite
+brew install postgresql
 
 # Install other useful binaries.
-brew install ack
 brew install dark-mode
-#brew install exiv2
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
+brew install imagemagick
 brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
 brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
 brew install tree
-brew install vbindiff
 brew install webkit2png
-brew install zopfli
+brew install ffmpeg
+brew install keybase
+
+# Install cask and apps.
+brew install cask
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install sublime-text3
+brew cask install iconping
+brew cask install slack
+brew cask install iterm2
+brew cask install 1password
+brew cask install disk-inventory-x
+brew cask install imageoptim
+brew cask install limechat
+brew cask install postgres
 
 # Remove outdated versions from the cellar.
 brew cleanup
